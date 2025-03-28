d9eff6f90eb7d1229c0a9407523931462bc01584
Santiago Javier Bolivar Salmaso <sbolivar@c2r15s1.42madrid.com>
2025-03-28 03:22:06 +0100

    signal1

--- include/Mini.h
+++ include/Mini.h
@@ -3,26 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   Mini.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dacastil <dacastil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sbolivar <sbolivar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 13:11:26 by dacastil          #+#    #+#             */
-/*   Updated: 2025/03/27 19:46:39 by dacastil         ###   ########.fr       */
+/*   Updated: 2025/03/28 03:09:07 by sbolivar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINI_H
 # define MINI_H
 
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <readline/readline.h>
-# include <readline/history.h>
 # include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
-char	*ft_strjoin(char const *s1, char const *s2);
-int		ft_strlen(const char *s);
-int		ft_strlcpy(char *dst, const char *src, size_t size);
+extern int	g_exit;
 
+char		*ft_strjoin(char const *s1, char const *s2);
+int			ft_strlen(const char *s);
+int			ft_strlcpy(char *dst, const char *src, size_t size);
 
 #endif
